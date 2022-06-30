a205933904eed50cba611d15fb489574853d905d
Juliana Mendes <juliana-dantas@live.com>
2022-06-30 20:17:45 -0300

    alteirei para valor diario

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 4.99
-const euro = 5.22
-const bitcoin = 0.62
 
-const convertValues = () => {
+const convertValues = async () => {
 	const inputReal = document.getElementById('input-real').value
 	const realValueText = document.getElementById('real-value')
 	const currencyValue = document.getElementById('currency-value-text')
+
+	const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+	const dolar = data.USDBRL.high
+	const euro = data.EURBRL.high
+	const bitcoin = data.BTCBRL.high
+
+	 console.log(data)
 
 	realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
 		style: 'currency',
